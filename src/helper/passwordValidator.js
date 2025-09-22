7e3cc15c0b36cf608845462d0c0f6778172cd3ba
vivektarun <vivektarun1234@gmail.com>
2025-09-22 21:29:37 +0530

    feat: basic form with basic validation is ready

---
+++ src/helper/passwordValidator.js
@@ -0,0 +1,14 @@
+const regex = new RegExp("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
+
+
+function validatePassword(string) {
+    if(string.length < 8) {
+        return false;
+    }
+    if(!regex.test(string)) {
+        return false;
+    }
+    return true;
+}
+
+export default validatePassword;
